a76556a0246eb70d9eb974d3b54762d06d7d7bc0
schneiderstag <brunopiasso@hotmail.com>
2021-02-18 23:37:59 +0000

    Add project files.

---
+++ Vega/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Controllers.Resources;
+using Vega.Models;
+
+namespace Vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Unidirectional Mappings (Left -> Right)
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
---
+++ Vega/Migrations/20210218222349_SeedDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 1 - Model A', (SELECT Id FROM Makes WHERE Name = 'Make 1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 1 - Model B', (SELECT Id FROM Makes WHERE Name = 'Make 1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 1 - Model C', (SELECT Id FROM Makes WHERE Name = 'Make 1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 2 - Model A', (SELECT Id FROM Makes WHERE Name = 'Make 2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 2 - Model B', (SELECT Id FROM Makes WHERE Name = 'Make 2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 2 - Model C', (SELECT Id FROM Makes WHERE Name = 'Make 2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 3 - Model A', (SELECT Id FROM Makes WHERE Name = 'Make 3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 3 - Model B', (SELECT Id FROM Makes WHERE Name = 'Make 3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make 3 - Model C', (SELECT Id FROM Makes WHERE Name = 'Make 3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Make 1', 'Make 2', 'Make 3')");
+            //migrationBuilder.Sql("DELETE FROM Models"); // When we delete from Makes all the associated models will also be deleted (Cascade).
+        }
+    }
+}
