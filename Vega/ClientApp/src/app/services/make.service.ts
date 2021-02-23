e63d3a616f648bbba19a0f0ede81214f857ad9fd
schneiderstag <brunopiasso@hotmail.com>
2021-02-23 22:41:34 +0000

    Add vehicle form, bootstrap and some folders.

---
+++ Vega/ClientApp/src/app/services/make.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MakeService {
+
+  constructor(private http: HttpClient) { }
+
+  getMakes() {
+    // By default the HttpClient deserializes the content of a response into an object.
+    // Some of its methods allow passing a generic type argument in order to duck - type the result.
+    // Thats why there is no json() method anymore.
+    //return this.http.get('api/makes').pipe(map(res => res.json()));
+    return this.http.get('api/makes');
+  }
+}
