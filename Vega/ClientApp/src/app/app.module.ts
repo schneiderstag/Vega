a70143640132356b8c598fbc1217f1f15e87a92a
schneiderstag <brunopiasso@hotmail.com>
2021-02-23 23:55:46 +0000

    Implement Make & Model dropdowns + organize folders

--- Vega/ClientApp/src/app/app.module.ts
+++ Vega/ClientApp/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 import { MakeService } from './services/make.service';
 
+import { AppComponent } from './components/app/app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,8 +23,8 @@ import { MakeService } from './services/make.service';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'vehicles/new', component: VehicleFormComponent },
---
+++ Vega/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MakeService } from '../../services/make.service';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes: any;
+  models: any[];
+  vehicle: any = {};
+
+  constructor(private makeService: MakeService) { }
+
+  // ngOnInit(): void {
+  //   this.makeService.getMakes().subscribe(makes => {
+  //     this.makes = makes
+  //     console.log("MAKES", this.makes);
+  //   });
+  // }
+  ngOnInit(): void {
+    this.makeService.getMakes().subscribe(makes => 
+      this.makes = makes);
+  }
+
+  onMakeChange() {
+    // console.log("VEHICLE", this.vehicle);
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
+}
