c7d842447a8395eb9ae32e592414041248a03bc2
schneiderstag <brunopiasso@hotmail.com>
2021-02-28 21:49:27 +0000

    Add contact and registration fields to vehicle form

--- Vega/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ Vega/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,6 +1,5 @@
-import { FeatureService } from './../../services/feature.service';
 import { Component, OnInit } from '@angular/core';
-import { MakeService } from '../../services/make.service';
+import { VehicleService } from '../../services/vehicle.service';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -13,8 +12,7 @@ export class VehicleFormComponent implements OnInit {
   features: any;
   vehicle: any = {};
 
-  constructor(private makeService: MakeService,
-              private featureService: FeatureService) { }
+  constructor(private vehicleService: VehicleService) { }
 
   // ngOnInit(): void {
   //   this.makeService.getMakes().subscribe(makes => {
@@ -23,10 +21,10 @@ export class VehicleFormComponent implements OnInit {
   //   });
   // }
   ngOnInit(): void {
-    this.makeService.getMakes().subscribe(makes => 
+    this.vehicleService.getMakes().subscribe(makes => 
       this.makes = makes);
 
-    this.featureService.getFeatures().subscribe(features =>
+    this.vehicleService.getFeatures().subscribe(features =>
       this.features = features)
   }
 
