32c8e5ce2de8a74a2b19ecd3277183089ba491cd
schneiderstag <brunopiasso@hotmail.com>
2021-03-02 23:46:11 +0000

    Create vehicle api

---
+++ Vega/Controllers/VehicleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vega.Models;
+
+namespace Vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehicleController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateVehicle(Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
+    }
+}
